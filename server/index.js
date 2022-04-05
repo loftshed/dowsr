9edf5312b94235b1f64c5aa6d19d131a22b9a446
Pete Löfstedt <pete.lofstedt@gmail.com>
2022-04-05 12:14:13 -0400

    Started server from scratch

---
+++ server/index.js
@@ -0,0 +1,40 @@
+"use strict";
+
+const express = require("express");
+const morgan = require("morgan");
+const cors = require("cors");
+const helmet = require("helmet");
+
+const app = express();
+
+/*-----------
+| handlers |
+-----------*/
+const {
+  // handlers here
+} = require("../server/userHandlers");
+
+app.use(morgan("tiny"));
+app.use(express.json()); // this was used in slingair server..  do i need?
+app.use(express.static("public")); // requests for static files go to public folder
+app.use(helmet());
+app.use(cors());
+
+/*-----------
+| endpoints |
+-----------*/
+
+/*------------------
+| end of endpoints |
+------------------*/
+
+app.get("*", (req, res) => {
+  res.status(404).json({
+    status: 404,
+    message:
+      "404: PEBCAK error occurred during development. Please contact support.",
+  });
+});
+
+// Sets server to listen with power level over 9000.
+app.listen(9001, () => console.log(`Listening on port 9001`));
