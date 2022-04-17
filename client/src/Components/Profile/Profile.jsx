a656ad6c622710be6768abf6e984db782ec49ea7
Pete Löfstedt <pete.lofstedt@gmail.com>
2022-04-17 19:19:14 -0400

    beefing up profile..

--- client/src/Components/Profile/Profile.jsx
+++ client/src/Components/Profile/Profile.jsx
@@ -1,14 +1,19 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 import {
   centeredFlexColumn,
   centeredFlexRow,
   fillSpace,
   TextButton,
 } from "../../styling/sharedstyles";
-import { BurgerMenuIcon } from "../../styling/react-icons";
+import {
+  BurgerMenuIcon,
+  HazardIcon,
+  PoliceIcon,
+  ToiletIcon,
+  WaterIcon,
+} from "../../styling/react-icons";
 import { SIZES } from "../../styling/constants";
 import ResponsiveContainer from "../../styling/ResponsiveContainer";
-
 import { useEffect, useContext, useState } from "react";
 import { useAuth0 } from "@auth0/auth0-react";
 import Flag from "react-world-flags";
@@ -17,11 +22,15 @@ import { AppContext } from "../../AppContext";
 import { getUser, getUserByUsername } from "../Auth/helpers";
 import LoadingSpinner from "../../styling/LoadingSpinner";
 import { useNavigate, useParams } from "react-router-dom";
+import { startThreadWithUser } from "../Messaging/helpers";
 import {
-  newThread,
-  startThreadWithUser,
-  getUserThreads,
-} from "../Messaging/helpers";
+  RiGlobeLine as GlobeIcon,
+  RiUserFollowLine as FollowIcon,
+} from "react-icons/ri";
+import { SendIcon } from "../../styling/react-icons";
+
+//FIXME: Literally everything is a disaster but I am in a mega super rush
+//TODO: Redesign this component from the ground up after all other features are complete
 
 const Profile = () => {
   const { loggedInUser, viewedProfile, setViewedProfile } =
@@ -30,7 +39,6 @@ const Profile = () => {
   const navigate = useNavigate();
   const params = useParams();
   const isOwnProfile = loggedInUser.username === params.username;
-  console.log(params);
 
   const handleGetProfile = async (username) => {
     if (!username) {
@@ -82,51 +90,113 @@ const Profile = () => {
               <Flag code={country} height={16} />
             </DetailsHeading>
             <Details>
-              <Location>{`${city}, ${region}`}</Location>
-              <DetailList>
-                <li>{contributions?.length} contributions</li>
-                <li>Member since {dayjs(regDate).format("MMMM YYYY")}</li>
-              </DetailList>
+              <Item>
+                <GlobeIcon />
+                {`${city}, ${region}`}
+              </Item>
+
+              {!isOwnProfile && (
+                <Item style={{ justifyContent: "space-between" }}>
+                  <ProfileButton
+                    onClick={async () => {
+                      const result = await startThreadWithUser(
+                        loggedInUser._id,
+                        _id,
+                        "👋",
+                        loggedInUser.username
+                      );
+                      navigate("/messages", { replace: true });
+                    }}
+                  >
+                    <span>Message</span>
+                    {"   "}
+                    <SendIcon style={{ display: "inline" }} />
+                  </ProfileButton>
+                  <ProfileButton>
+                    <FollowIcon style={{ display: "inline" }} />
+                    {"   "}
+                    <span>Follow</span>
+                  </ProfileButton>
+                </Item>
+              )}
+              <BottomContainer isOwnProfile={isOwnProfile}>
+                <span style={{ fontSize: "12px", padding: "5px" }}>
+                  Member since {dayjs(regDate).format("MMMM YYYY")}
+                </span>
+                <ItemContainer>
+                  <Item style={{ justifyContent: "center" }}>
+                    <span>{contributions?.length}</span> pin contribution
+                    {contributions?.length === 1 ? "" : "s"}
+                  </Item>
+                  <Contributions>
+                    <Column>
+                      <Icon color={"#48cae4"}>
+                        <WaterIcon />
+                      </Icon>
+                      <Number>2</Number>
+                    </Column>
+                    <Column>
+                      <Icon color={"#b2967d"}>
+                        <ToiletIcon />
+                      </Icon>
+                      <Number>2</Number>
+                    </Column>
+                    <Column>
+                      <Icon color={"#e76f51"}>
+                        <HazardIcon />
+                      </Icon>
+                      <Number>1</Number>
+                    </Column>
+                    <Column>
+                      <Icon color={"#db7f8e"}>
+                        <PoliceIcon />
+                      </Icon>
+                      <Number>6</Number>
+                    </Column>
+                  </Contributions>
+                </ItemContainer>
+              </BottomContainer>
             </Details>
           </UserDetails>
-          <Actions>
-            {!isOwnProfile && (
-              <TextButton
-                onClick={async () => {
-                  const result = await startThreadWithUser(
-                    loggedInUser._id,
-                    _id,
-                    "👋",
-                    loggedInUser.username
-                  );
-                  navigate("/messages", { replace: true });
-                }}
-              >
-                Send Message
-              </TextButton>
-            )}
-          </Actions>
         </InnerContainerLiner>
       </InnerContainer>
     </ResponsiveContainer>
   );
 };
 
+{
+  /*  */
+}
+
 export default Profile;
 
-const Actions = styled.div`
-  display: flex;
-  width: 100%;
+const Icon = styled.div`
+  width: fit-content;
   height: fit-content;
-  padding: 3px 20px;
-  justify-content: flex-end;
+  padding: 8px;
   background-color: var(--color-less-dark-grey);
-
-  button {
-    border-radius: unset;
-    padding: 2px 5px;
-    background-color: var(--color-darkest-grey);
+  border-radius: 50%;
+  border: 2px solid ${(props) => props.color};
+  svg {
+    fill: ${(props) => props.color};
   }
+  @media (min-width: 450px) {
+    padding: 12px;
+  }
+`;
+
+const sharedDetailStyle = css`
+  background-color: var(--color-less-dark-grey);
+  padding: 0 ${SIZES.universalPadding}px;
+  width: 100%;
+  font-size: 14px;
+  @media (min-width: 450px) {
+    padding: 2.5px 40px;
+    font-size: 20px;
+  }
+  border-top: 1px solid var(--color-super-dark-grey);
+  border-bottom: 1px solid var(--color-super-dark-grey);
+  display: flex;
 `;
 
 const InnerContainer = styled.div`
@@ -143,33 +213,17 @@ const InnerContainerLiner = styled.div`
   border-radius: ${SIZES.borderRadius}px;
 `;
 
-const ProfileChin = styled.div`
-  background-color: var(--color-less-dark-grey);
-  border-bottom-left-radius: ${SIZES.borderRadius}px;
-  border-bottom-right-radius: ${SIZES.borderRadius}px;
-  /* padding: 5px ${SIZES.universalPadding}px; */
+const ProfileSplash = styled.div`
+  display: flex;
+  position: relative;
   width: 100%;
-`;
-
-const Location = styled.div`
-  background-color: var(--color-less-dark-grey);
-  padding-left: ${SIZES.universalPadding}px;
-  width: 100%;
-  font-size: 14px;
+  height: 150px;
+  background-image: url("/bg.jpg");
+  border-top-left-radius: ${SIZES.borderRadius}px;
+  border-top-right-radius: ${SIZES.borderRadius}px;
   @media (min-width: 450px) {
-    padding: 2.5px ${SIZES.leftPaddingLrg}px;
-    font-size: 20px;
+    height: 250px;
   }
-  border-top: 1px solid var(--color-super-dark-grey);
-  border-bottom: 1px solid var(--color-super-dark-grey);
-`;
-
-const UserDetails = styled.div`
-  ${centeredFlexColumn}
-  flex-grow: 1;
-  width: 100%;
-  border-radius: ${SIZES.borderRadius}px;
-  border-top: 1px solid var(--color-super-dark-grey);
 `;
 
 const Avatar = styled.img`
@@ -184,7 +238,7 @@ const Avatar = styled.img`
     width: 150px;
     height: 150px;
     bottom: -100px;
-    right: 50px;
+    right: 40px;
   }
   box-shadow: 1.8px 1.6px 4px rgba(0, 0, 0, 0.02),
     4.3px 3.8px 9.6px rgba(0, 0, 0, 0.028),
@@ -195,28 +249,28 @@ const Avatar = styled.img`
   outline: 1px solid var(--color-super-dark-grey);
 `;
 
-const ProfileSplash = styled.div`
-  display: flex;
-  position: relative;
+const UserDetails = styled.div`
+  ${centeredFlexColumn}
+  flex-grow: 1;
   width: 100%;
-  height: 150px;
-  background-image: url("/bg.jpg");
-  border-top-left-radius: ${SIZES.borderRadius}px;
-  border-top-right-radius: ${SIZES.borderRadius}px;
+  border-radius: ${SIZES.borderRadius}px;
+  border-top: 1px solid var(--color-super-dark-grey);
 `;
 
 const DetailsHeading = styled.div`
   ${centeredFlexRow}
   justify-content: flex-start;
   background-color: var(--color-darkest-grey);
-  padding-left: ${SIZES.universalPadding}px;
+  padding: ${SIZES.universalPadding}px;
   gap: 10px;
   height: 50px;
   width: 100%;
   @media (min-width: 450px) {
+    padding: 40px;
+    height: 75px;
     font-size: 22px;
     h3 {
-      font-size: 28px;
+      font-size: 34px;
     }
   }
 `;
@@ -235,11 +289,71 @@ const Details = styled.div`
   }
 `;
 
-const DetailList = styled.div`
-  padding: unset;
-  padding: 25px;
+const Item = styled.div`
+  gap: 3px;
+  align-items: center;
+  width: 100%;
+  height: 30px;
+  ${sharedDetailStyle}
+  @media (min-width: 450px) {
+    height: 40px;
+    font-size: 22px;
+  }
 `;
 
-const SendMessage = styled(TextButton)`
+const ProfileButton = styled(TextButton)`
+  border-radius: 3px;
+  padding: 0px 5px;
   cursor: pointer;
+  gap: 3px;
+`;
+
+const Number = styled.div`
+  line-height: 12px;
+`;
+
+const Column = styled.div`
+  ${centeredFlexColumn};
+  gap: 7.5px;
+  @media (min-width: 450px) {
+    gap: 15px;
+  }
+`;
+
+const Row = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 3px;
+`;
+
+const Contributions = styled.div`
+  display: flex;
+  justify-content: space-around;
+  padding: 15px;
+  @media (min-width: 450px) {
+    padding: 20px 20px 25px 20px;
+  }
+`;
+
+const ItemContainer = styled.div`
+  width: 100%;
+`;
+
+const BottomContainer = styled.div`
+  justify-content: flex-end;
+  align-items: center;
+  display: flex;
+  flex-direction: column;
+  height: calc(100% - 60px);
+  width: 100%;
+  @media (min-width: 450px) {
+    height: calc(100% - 80px);
+  }
+  ${(props) =>
+    props.isOwnProfile &&
+    css`
+  height: calc(100% - 30px);
+  @media (min-width: 450px) {
+    height: calc(100% - 40px);
+  `}
 `;
--- client/src/Styling/react-icons.js
+++ client/src/Styling/react-icons.js
@@ -11,17 +11,19 @@ import {
   BsFillSaveFill,
   BsFillHandThumbsUpFill,
   BsFillHandThumbsDownFill,
+  BsConeStriped,
 } from "react-icons/bs";
 import { TiThMenu } from "react-icons/ti";
 import { IoLogOut, IoSend } from "react-icons/io5";
 import { IoIosWater } from "react-icons/io";
-import { FaToilet } from "react-icons/fa";
+import { FaToilet, FaPoop } from "react-icons/fa";
 import {
   MdPedalBike,
   MdCoffee,
   MdLocalGroceryStore,
   MdFilterAlt,
   MdAdminPanelSettings,
+  MdLocalPolice,
 } from "react-icons/md";
 import { BiTrafficCone } from "react-icons/bi";
 import {
@@ -29,6 +31,7 @@ import {
   RiMapPinAddFill,
   RiCloseCircleFill,
 } from "react-icons/ri";
+import { GiPoliceBadge } from "react-icons/gi";
 
 const CircledArrowRight = styled(BsArrowRightCircleFill)``;
 const SearchIcon = styled(BsSearch)``;
@@ -44,13 +47,13 @@ const SendIcon = styled(IoSend)``;
 const FilterIcon = styled(MdFilterAlt)``;
 const BikeIcon = styled(MdPedalBike)``;
 const CoffeeIcon = styled(MdCoffee)``;
-const HazardIcon = styled(BiTrafficCone)``;
-const PoliceIcon = styled(RiPoliceCarFill)``;
+const HazardIcon = styled(BsConeStriped)``;
+const PoliceIcon = styled(GiPoliceBadge)``;
 const StoreIcon = styled(MdLocalGroceryStore)``;
 const CreatePinIcon = styled(RiMapPinAddFill)``;
 const CloseIcon = styled(RiCloseCircleFill)``;
 const WaterIcon = styled(IoIosWater)``;
-const ToiletIcon = styled(FaToilet)``;
+const ToiletIcon = styled(FaPoop)``;
 const AdminIcon = styled(MdAdminPanelSettings)``;
 const BurgerMenuIcon = styled(TiThMenu)``;
 const ThumbsUpIcon = styled(BsFillHandThumbsUpFill)``;
