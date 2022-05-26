875d1daddf0f1ea8b89472406089b1c6add3d51e
Pete Löfstedt <pete.lofstedt@gmail.com>
2022-05-26 17:54:16 -0400

    deploying to heroku

--- server/server.js
+++ server/server.js
@@ -1,9 +1,9 @@
-"use strict";
+'use strict';
 
-const express = require("express");
-const morgan = require("morgan");
-const cors = require("cors");
-const helmet = require("helmet");
+const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+const helmet = require('helmet');
 
 // we assign the express function to a variable for later use
 // this allows us to use the express function without having to
@@ -16,13 +16,13 @@ const app = express();
 ---------*/
 // morgan is a logger. it logs the request and response to the console.
 // the argument "tiny" is the format.
-app.use(morgan("tiny"));
+app.use(morgan('tiny'));
 
 // express.json() is a middleware that parses incoming requests with JSON payloads.
 app.use(express.json());
 
 // specifies the static folder where the frontend files are located to make them accessible.
-app.use(express.static("public"));
+app.use(express.static('public'));
 
 // helmet is a security package that helps you secure your Express apps by setting various HTTP headers.
 app.use(helmet());
@@ -33,24 +33,24 @@ app.use(cors());
 /*----------------------------------------
 | socket.io stuff i understand even less |
 ----------------------------------------*/
-const http = require("http").createServer(app);
-const io = require("socket.io")(http, {
-  cors: { origin: "*" },
-  methods: ["GET", "POST"],
-});
-http.listen(8080, () => console.log("listening on http://localhost:8080"));
+// const http = require("http").createServer(app);
+// const io = require("socket.io")(http, {
+//   cors: { origin: "*" },
+//   methods: ["GET", "POST"],
+// });
+// http.listen(8080, () => console.log("listening on http://localhost:8080"));
 
-io.on("connection", (socket) => {
-  console.log("a user connected");
-  socket.on("postMessage", async ({ post }) => {
-    try {
-      const returnData = {
-        message: "yo",
-      };
-      io.emit("newMessage", returnData);
-    } catch (error) {}
-  });
-});
+// io.on("connection", (socket) => {
+//   console.log("a user connected");
+//   socket.on("postMessage", async ({ post }) => {
+//     try {
+//       const returnData = {
+//         message: "yo",
+//       };
+//       io.emit("newMessage", returnData);
+//     } catch (error) {}
+//   });
+// });
 
 // io.on("connection", (socket) => {
 //   console.log(socket.handshake.query); // prints { x: "42", EIO: "4", transport: "polling" }
@@ -59,18 +59,17 @@ io.on("connection", (socket) => {
 /*--------
 | routes |
 --------*/
-app.use(require("./src/routes/chat"));
-app.use(require("./src/routes/map"));
-app.use(require("./src/routes/user"));
+app.use(require('./src/routes/chat'));
+app.use(require('./src/routes/map'));
+app.use(require('./src/routes/user'));
 
 // if no route is found, return a 404 error.
-app.get("*", (req, res) => {
+app.get('*', (req, res) => {
   res.status(404).json({
     status: 404,
-    message:
-      "404: PEBCAK error occurred during development. Please contact support.",
+    message: '404: PEBCAK error occurred during development. Please contact support.',
   });
 });
 
 // sets server to listen with power level over 9000.
-app.listen(9001, () => console.log(`Listening on port 9001`));
+app.listen(process.env.PORT || 9001, () => console.log(`Listening on port 9001`));
