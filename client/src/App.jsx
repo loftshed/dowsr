a7c77b0c578856bacce937139bf2b9e6085eccd8
Pete Löfstedt <pete.lofstedt@gmail.com>
2022-05-26 19:14:16 -0400

    changing folder case

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,30 +1,27 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { useContext, useEffect } from "react";
-import styled from "styled-components/macro";
-
-import { centeredFlexColumn } from "./styling/sharedstyles";
-import { getUser } from "./components/Auth/helpers";
-
-import { AppContext } from "./components/AppContext";
-import { Admin } from "./components/Admin";
-import Error from "./components/Error";
-import FirstLogin from "./components/Auth/FirstLogin";
-import GlobalStyle from "./styling/GlobalStyles";
-import Header from "./components/Header";
-import { Map } from "./components/Map";
-import { Menu } from "./components/Menu";
-import Chat from "./components/Chat";
-import Notifications from "./components/Notifications";
-import Profile from "./components/Profile";
-import Theme from "./AppTheme";
-import ThemeMUI from "./AppTheme.mui";
-import { useAuth0 } from "@auth0/auth0-react";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { useContext, useEffect } from 'react';
+import styled from 'styled-components/macro';
+import { centeredFlexColumn } from './styling/sharedstyles';
+import { getUser } from './components/Auth/helpers';
+import { AppContext } from './components/AppContext';
+import { Admin } from './components/Admin';
+import Error from './components/Error';
+import FirstLogin from './components/Auth/FirstLogin';
+import GlobalStyle from './styling/GlobalStyles';
+import Header from './components/Header';
+import { Map } from './components/Map';
+import { Menu } from './components/Menu';
+import Chat from './components/Chat';
+import Notifications from './components/Notifications';
+import Profile from './components/Profile';
+import Theme from './AppTheme';
+import ThemeMUI from './AppTheme.mui';
+import { useAuth0 } from '@auth0/auth0-react';
 
 //TODO: ULTRA MEGA TODO: Redo signin flow!!
 
 const App = () => {
-  const { loggedInUser, setLoggedInUser, setFirstLogin } =
-    useContext(AppContext);
+  const { loggedInUser, setLoggedInUser, setFirstLogin } = useContext(AppContext);
   // const [firstLogin, setFirstLogin] = useState(null);
 
   const { user } = useAuth0();
@@ -36,16 +33,14 @@ const App = () => {
         if (!user || loggedInUser) return;
 
         // If user is available from Auth0, but still not logged in, check localStorage for user object.
-        const locallyStoredUser = JSON.parse(
-          localStorage.getItem("locallyStoredUser")
-        );
+        const locallyStoredUser = JSON.parse(localStorage.getItem('locallyStoredUser'));
         // If user is available from localStorage, set loggedInUser to user from localStorage.
         if (locallyStoredUser) {
           setLoggedInUser(locallyStoredUser);
           return;
         }
         // If the user is not available in localStorage, get user from backend.
-        const { data, userFound } = await getUser("email", user.email);
+        const { data, userFound } = await getUser('email', user.email);
 
         // If user is found in backend, It must be the user's first login.
         if (!userFound) {
@@ -55,7 +50,7 @@ const App = () => {
         }
         // If none of the early returns were triggered, we have a user,
         setFirstLogin(false); // And we know it is not the user's first login.
-        localStorage.setItem("locallyStoredUser", JSON.stringify(data)); // So, store the user in localStorage.
+        localStorage.setItem('locallyStoredUser', JSON.stringify(data)); // So, store the user in localStorage.
         setLoggedInUser(data); // Then set the loggedInUser state to the user found in the database.
       } catch (err) {
         console.log(err);
